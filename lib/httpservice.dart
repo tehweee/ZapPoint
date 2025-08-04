4ec67c3f361e6201981c852a4273134ca11ac6b8
tehweee <pangtehwee11@gmail.com>
2025-08-04 13:45:18 +0800

    ge

--- lib/httpservice.dart
+++ lib/httpservice.dart
@@ -4,20 +4,22 @@ import 'Chargers.dart';
 
 class HttpService {
   static const String _baseUrl =
-      'https://ev-charge-finder.p.rapidapi.com/search-by-coordinates-point?lat=1.438&lng=103.786&limit=20';
+      'https://ev-charge-finder.p.rapidapi.com/search-by-coordinates-point?lat=1.3048&lng=103.8318&limit=20';
 
   static const Map<String, String> _headers = {
-    'x-rapidapi-key': 'f8f1cc1c8emsh8ce076a6136ca13p116ff2jsn1f2f9b873075',
+    'x-rapidapi-key': 'f72197243bmshf94cb6e9a6ce889p14da50jsn6ed480952fb4',
     'x-rapidapi-host': 'ev-charge-finder.p.rapidapi.com',
   };
 
   static Future<List<Datum>?> getCarparks() async {
     try {
       final response = await http.get(Uri.parse(_baseUrl), headers: _headers);
-
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
       if (response.statusCode == 200) {
         final jsonBody = json.decode(response.body);
         final data = jsonBody['data'] as List;
+        print('Parsed ${data.length} chargers');
         return data.map((item) => Datum.fromJson(item)).toList();
       } else {
         print('Failed with status: ${response.statusCode}');
